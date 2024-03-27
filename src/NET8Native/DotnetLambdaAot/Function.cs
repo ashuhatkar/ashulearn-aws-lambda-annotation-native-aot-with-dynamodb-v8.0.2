f0db04962a5414d88afd6add4d1efb00a76b3594
Ashish <ashish.hatkar@gmail.com>
2024-03-27 16:20:59 +0530

    Fix to handlers

--- src/NET8Native/DotnetLambdaAot/Function.cs
+++ src/NET8Native/DotnetLambdaAot/Function.cs
@@ -20,13 +20,8 @@ using Amazon.Lambda.Annotations;
 using Amazon.Lambda.Annotations.APIGateway;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
-using Amazon.Lambda.Serialization.SystemTextJson;
 using Shared.DataAccess;
 using Shared.Models;
-
-//Assembly attributes to enable the lambda function's JSON input to be converted into a .NET class
-//[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
-//[assembly: LambdaSerializer(typeof(SourceGeneratorLambdaJsonSerializer<LambdaFunctionJsonSerializerContext>))]
 
 namespace DotNetLambdaAot;
 
